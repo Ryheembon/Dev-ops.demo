10ac87a21386a1ac8be2deb2e24975501622962b
Ryheembon <your-email@example.com>
2025-05-15 20:36:00 -0400

    Initial commit

---
+++ src/components/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Welcome() {
+    return (
+        <div className="welcome-container">
+            <h1>Welcome to DevOps Demo</h1>
+            <p>This is a simple demo application to showcase DevOps concepts.</p>
+        </div>
+    )
+}
+
+export default Welcome;
